e0dae03bf1980068d1c5514fda8f9f5ccc085ce7
Tao Wang <twang2218@gmail.com>
2016-05-04 01:51:58 +1000

    Add Google Geolocation API

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+// `config.json`:
+// {
+//     "google_api_key": "<YOUR_GOOGLE_API_KEY>"
+// }
+const config = require('./config');
 
+const Geolocation = require('./lib');
+const locate = new Geolocation({
+  google_api_key: config.google_api_key,
+});
+
+const data = {
+  mcc: 460,
+  mnc: 1,
+  lac: 4219,
+  cid: 20925,
+};
+
+locate.google.locate(data, (error, location) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(location);
+    console.log(
+      `https://www.google.com/maps/@${location.latitude},${location.longitiude},1000m/data=!3m1!1e3`
+    );
+    console.log(`http://www.google.cn/maps/@${location.latitude},${location.longitiude},1000m/data=!3m1!1e3`);
+    console.log(`https://www.bing.com/maps/?v=2&cp=${location.latitude}~${location.longitiude}&style=h&lvl=12`);
+  }
+});
